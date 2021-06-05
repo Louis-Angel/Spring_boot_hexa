e326f4a643f21714184dc9967456b16884ad4b89
Louis Luna <systems_angel@hotmail.com>
2021-06-04 20:59:25 -0500

    stable and working

--- Accounts/src/main/java/com/flightapp/security/SecurityConfig.java
+++ Accounts/src/main/java/com/flightapp/security/SecurityConfig.java
@@ -3,52 +3,52 @@ package com.flightapp.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
 	@Autowired
 	private DataSource dataSource;
 	
-
-	
-	//Enable jdbc authentication
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-        .dataSource(dataSource)
-        .usersByUsernameQuery("SELECT email, password "
-          + "from user "
-          + "where email = ?")
-        .authoritiesByUsernameQuery("select email, role "
-          + "from user "
-          + "where email = ?");
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/").permitAll()
+		http.cors();
+		http.httpBasic()
+	    .and()
+	    .authorizeRequests()
 		.antMatchers("/api/v1.0/flight/airline/register").hasAnyRole("CUSTOMER", "ADMIN")
-		.antMatchers("/api/v1.0/flight/search").hasAnyRole("CUSTOMER")
-		.antMatchers("/api/v1.0/flight/airline/inventory/add").hasAnyRole("ADMIN")
-		.antMatchers("/api/v1.0/flight/admin/login").permitAll();
-
-		//http.csrf().disable();
+		.antMatchers("/api/v1.0/flight/airline/inventory/add").hasRole("ADMIN")
+		.antMatchers("/api/v1.0/flight/search").hasRole("CUSTOMER")
+		.antMatchers("/").permitAll()
+		.antMatchers("/api/v1.0/flight/admin/login").permitAll()
+		.anyRequest().authenticated()
+        .and()
+        .csrf().disable();
 	}
-
-    /*remove this in memory authentication configuration
-	 @Autowired
-	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-		authenticationMgr.inMemoryAuthentication()
-		.withUser("name1").password("admin").roles("ADMIN").and()
-		.withUser("name2").password("javainuse").("ROLE_USER", "ROLE_ADMIN");
-	} */
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception
+	{	 
+		authenticationMgr.jdbcAuthentication().dataSource(dataSource)
+		.authoritiesByUsernameQuery("SELECT user.email as username, role "
+		          + "from user "
+		          + "where user.email = ?")
+		.usersByUsernameQuery("SELECT user.email as username, password, enabled "
+		          + "from user "
+		          + "where user.email = ?");
+	} 
 }
